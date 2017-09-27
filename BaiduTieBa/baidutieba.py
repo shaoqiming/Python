b0eadb1349d41a949ab5d2b2443a1ab99b9d8ca2
shaoqiming <549730265@qq.com>
2017-09-27 20:38:39 +0800

    将贴吧信息写入文件中

--- BaiduTieBa/baidutieba.py
+++ BaiduTieBa/baidutieba.py
@@ -31,28 +31,31 @@ class BDTB:
     def getContent(self,page):
         pattern=re.compile('<div id="post_content_.*?>(.*?)</div>',re.S)
         items=re.findall(pattern,page)
+        content=[]
         for item in items:
-            print "==========================="
-            print self.tool.repalce(item)
-            print "==========================="
+            content.append(("\n"+self.tool.repalce(item)+"\n"))
+        return content
 
-    def getTitle(self):
-        pagecode=self.getPage(1)
+    def getTitle(self,pagecode):
+        #pagecode=self.getPage(1)
         pattern=re.compile('<h3 class="core_title_txt.*?>(.*?)</h3>',re.S)
         result=re.search(pattern,pagecode)
         if result:
             print result.group(1).strip()
+            return result.group(1).strip().decode("utf-8")
         else:
             print "没有结果"
 
-    def getPageNum(self):
-        pagecode=self.getPage(1)
+    def getPageNum(self,pagecode):
+        #pagecode=self.getPage(1)
         pattern=re.compile('<ul class="l_posts_num".*?<span class="red">(.*?)</span>',re.S)
         result=re.search(pattern,pagecode)
         if result:
             print result.group(1)
+            return  result.group(1)
         else:
             print "没有搜索到页数"
+
 
     def setFileTitle(self,title):
         if title is not None:
@@ -60,16 +63,35 @@ class BDTB:
         else:
             self.file=open(self.defaultTitle+".txt","w+")
 
+    def writerData(self,content):
+        for i in content:
+            self.file.write(i)
+
+
     def start(self):
         indexPage=self.getPage(1)
-
-
-
-
+        pageNum=self.getPageNum(indexPage)
+        title=self.getTitle(indexPage)
+        self.setFileTitle(title)
+        if  pageNum==None:
+            print "URL已经失效，请重试"
+            return
+        else:
+            try:
+                print "该帖子共有"+str(pageNum)+"页"
+                for i in range(1,int(pageNum)+1):
+                    print "正在写入第"+str(i)+"页"
+                    page=self.getPage(i)
+                    content=self.getContent(page)
+                    self.writerData(content)
+            except IOError,e:
+                print "写入异常，原因："+e.message
+            finally:
+                self.file.close()
+                print "写入完成"
 
 
 baseUrl='https://tieba.baidu.com/p/3138733512'
-bdtb=BDTB(baseUrl,1)
-pagecode=bdtb.getPage(1)
-bdtb.getContent(pagecode)
 
+bdtb=BDTB(baseUrl,1)
+bdtb.start()
